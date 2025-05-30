6a294e4fb755e17f149ab30cab4c236233d13e9b
cantelerai <vieira.raiany@escola.pr.gov.br>
2025-05-30 09:37:37 -0300

    t

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-let contador = 0;
-const contadorElemento = document.getElementById('contador');
-const botao = document.getElementById('botaoClique');
+document.addEventListener('DOMContentLoaded', function () {
+  let contador = 0;
+  const contadorElemento = document.getElementById('contador');
+  const botao = document.getElementById('botaoClique');
 
-botao.addEventListener('click', () => {
-  contador++;
-  contadorElemento.textContent = contador;
+  botao.addEventListener('click', () => {
+    contador++;
+    contadorElemento.textContent = contador;
+  });
 });
