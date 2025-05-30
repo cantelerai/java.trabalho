256cb3903098d86d74e27cda7df6d0b4c4a604df
cantelerai <vieira.raiany@escola.pr.gov.br>
2025-05-30 11:38:59 -0300

    f

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-// Espera o HTML carregar antes de executar
-document.addEventListener('DOMContentLoaded', () => {
-  let contador = 0;
+let contador = 0;
 
-  const contadorElemento = document.getElementById('contador');
-  const botaoClique = document.getElementById('botaoClique');
-  const botaoReiniciar = document.getElementById('botaoReiniciar');
-
-  botaoClique.addEventListener('click', () => {
-    contador++;
-    contadorElemento.textContent = contador;
-  });
-
-  botaoReiniciar.addEventListener('click', () => {
-    contador = 0;
-    contadorElemento.textContent = contador;
-  });
-});
+function incrementar() {
+  contador++;
+  document.getElementById('contador').innerText = contador;
+}
